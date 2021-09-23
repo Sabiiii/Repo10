cd0d671aba973360270591465f0d492c62c14a18
Sabiiii <sabinaajanovic27@gmail.com>
2021-09-23 09:34:49 +0400

    Adding objects v2

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -1,17 +1,21 @@
-var myPet = {
+
+myPet = { 
 
     species: "Huskey",
     name: "Njusko",
     legs: 4,
-    friends: "Zeko", "Medo"
+    friends: ["medo", "zeko"] 
 
 };
 
-function myFunction(myObj){
+function myFunction(myObj) {
 
     return myObj;
 }
 
-
 console.log(myFunction(myPet));
-module.exports = {mypet, myFunction};
+module.exports = {myPet, myFunction}
+
+
+
+
